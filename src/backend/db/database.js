897480041036aa7fec66221461aee0af87e765e0
Bob Kunanda <19623153@std.stei.itb.ac.id>
2025-01-13 11:25:12 +0700

    added: backend

---
+++ src/backend/db/database.js
@@ -0,0 +1,11 @@
+const sqlite3 = require('sqlite3').verbose();
+
+const db = new sqlite3.Database('./todo.db', (err) => {
+    if (err) {
+        console.error('Failed to connect to SQLite database:', err.message);
+    } else {
+        console.log('Connected to the SQLite database.');
+    }
+});
+
+module.exports = db;
